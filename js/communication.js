92808f97e13f176a56ca96f8a304fb314742db3b
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-08-29 13:30:37 +0200

    add control panel

---
+++ js/communication.js
@@ -0,0 +1,9 @@
+function onConnect() {
+    let element = document.getElementById("userResponseField")
+    element.innerHTML = "Polaczenie jeszcze nie zaimplementowane!"
+}
+
+function onDisconnect() {
+    let element = document.getElementById("userResponseField")
+    element.innerHTML = "Rozlaczenie jeszcze nie zaimplementowane!"
+}
