8445809da5d8adbb0beb9870b66756f523f6a56d
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-08-29 15:42:35 +0200

    add board + plan.img

--- js/script.js
+++ js/script.js
@@ -1,15 +1,74 @@
-const WIDTH = 800;
-const HEIGHT = 600;
+const WIDTH = 800
+const HEIGHT = 600
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(WIDTH,HEIGHT);
-ctx.stroke();
+const UPPER_LINE = HEIGHT/6
+const LEFT_LINE = WIDTH*(3/16)
+const RIGHT_LINE = WIDTH*(13/16)
 
-ctx.beginPath();
-ctx.arc(WIDTH/2,HEIGHT/2,40,0,2*Math.PI);
-ctx.stroke();
+const GAME_BOARD = HEIGHT-UPPER_LINE // == RIGHT_LINE - LEFT_LINE
+const SQUARE = GAME_BOARD / 11
 
-ctx.font = "30px Arial";
-ctx.fillText("Bomberman!", WIDTH/2,HEIGHT/8);
+let c = document.getElementById("myCanvas")
+let ctx = c.getContext("2d")
+
+drawBackground()
+
+function drawBackground() {
+    // GORNA LINIA
+    ctx.moveTo(0,UPPER_LINE)
+    ctx.lineTo(WIDTH,UPPER_LINE)
+    ctx.stroke()
+
+    // LEWY PANEL
+    ctx.moveTo(LEFT_LINE,UPPER_LINE)
+    ctx.lineTo(LEFT_LINE,HEIGHT)
+    ctx.stroke()
+
+    // PRAWY PANEL
+    ctx.moveTo(RIGHT_LINE,UPPER_LINE)
+    ctx.lineTo(RIGHT_LINE,HEIGHT)
+    ctx.stroke()
+
+    // GREEN BACKGROUND
+    ctx.fillStyle = "#00cc66"
+    ctx.fillRect(LEFT_LINE, UPPER_LINE, RIGHT_LINE-LEFT_LINE, HEIGHT-UPPER_LINE)
+
+    // WALL BLOCKS
+    let wall = new Image(SQUARE,SQUARE)
+    wall.onload = function() {
+        
+        for(let i=0;i<11;i++) {
+            let posX = LEFT_LINE
+            let posY = UPPER_LINE + i*SQUARE
+            ctx.drawImage(wall, posX, posY, this.width, this.height)
+        }
+
+        for(let i=0;i<11;i++) {
+            let posX = RIGHT_LINE - SQUARE
+            let posY = UPPER_LINE + i*SQUARE
+            ctx.drawImage(wall, posX, posY, this.width, this.height)
+        }
+
+        for(let i=0;i<11;i++) {
+            let posX = LEFT_LINE + i*SQUARE
+            let posY = UPPER_LINE
+            ctx.drawImage(wall, posX, posY, this.width, this.height)
+        }
+
+        for(let i=0;i<11;i++) {
+            let posX = LEFT_LINE + i*SQUARE
+            let posY = HEIGHT - SQUARE
+            ctx.drawImage(wall, posX, posY, this.width, this.height)
+        }
+
+        for(let i=0;i<4;i++) {
+            for(let j=0;j<4;j++) {
+                let posX = LEFT_LINE + 2*SQUARE + i*2*SQUARE
+                let posY = UPPER_LINE + 2*SQUARE + j*2*SQUARE
+                ctx.drawImage(wall, posX, posY, this.width, this.height)
+            }
+        }
+
+    }
+    wall.src = "sprites/wall.png"
+}
