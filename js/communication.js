6aee2bdc769b57969047745488636bf8391fd0b4
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-08-30 17:10:47 +0200

    szkielet komunikacji

--- js/communication.js
+++
@@ -1,9 +0,0 @@
-function onConnect() {
-    let element = document.getElementById("userResponseField")
-    element.innerHTML = "Polaczenie jeszcze nie zaimplementowane!"
-}
-
-function onDisconnect() {
-    let element = document.getElementById("userResponseField")
-    element.innerHTML = "Rozlaczenie jeszcze nie zaimplementowane!"
-}
--- js/script.js
+++ js/script.js
@@ -5,18 +5,14 @@ const UPPER_LINE = HEIGHT/6
 const LEFT_LINE = WIDTH*(3/16)
 const RIGHT_LINE = WIDTH*(13/16)
 
-let boardSize = 11 // Musi byc nieprzyste
+let boardSize = 7 // Musi byc nieprzyste
 
 const GAME_BOARD = HEIGHT-UPPER_LINE // == RIGHT_LINE - LEFT_LINE
 const SQUARE = GAME_BOARD / boardSize
 
 let c = document.getElementById("myCanvas")
 let ctx = c.getContext("2d")
-
-let cBuffer = document.getElementById("myCanvas")
-cBuffer.width = WIDTH
-cBuffer.height = HEIGHT
-let ctxBuffer = c.getContext("2d")
+let websocket;
 
 let playersPos = [
     [0, 0],
@@ -25,11 +21,10 @@ let playersPos = [
     [0, 0]
   ];
 
-window.setInterval(game,10)
+window.setInterval(gameLoop,5)
 
 
 document.addEventListener('keydown', function(event) {
-    console.log("EELO")
     let key = event.which
     if(key === 37) {
         playersPos[0][0] = playersPos[0][0] - 1
@@ -42,39 +37,78 @@ document.addEventListener('keydown', function(event) {
     }
 })
 
-function game() {
-
+function gameLoop() {
     drawBackground(boardSize,boardSize)
     drawPlayers()
-    ctx.drawImage(cBuffer)
 }
 
-// RYSUJE TYLKO 1 GRACZA
+function onConnect() {
+    websocket = new WebSocket(document.myform.serverAddressImput.value);
+    websocket.onopen = function(evt) { onOpen(evt) };
+    websocket.onclose = function(evt) { onClose(evt) };
+    // websocket.onmessage = function(evt) { onMessage(evt) };
+    // websocket.onerror = function(evt) { onError(evt) };
+}
+
+function onOpen(evt) {
+    document.getElementById("userResponseField").innerHTML = "Trwa łączenie!"
+	document.myform.connectButton.disabled = true;
+	document.myform.disconnectButton.disabled = false;
+}
+
+function onClose(evt) {
+    document.getElementById("userResponseField").innerHTML = "Zerwano polaczenie!"
+	document.myform.connectButton.disabled = false;
+	document.myform.disconnectButton.disabled = true;
+}
+
+function onMessage(evt) {
+    // TO DO
+    // OBSLUGA PRZYCHODZACYCH POLECEN
+    // DUZO ROBOTY!!!
+    console.log(evt.data)
+}
+
+function onError(evt) {
+	websocket.close();
+    document.getElementById("userResponseField").innerHTML = "Wystapił błąd!"
+	document.myform.connectButton.disabled = false;
+	document.myform.disconnectButton.disabled = true;
+}
+
+// TO DO
+function doSend(message) {
+    websocket.send(message);
+  }
+
+// RYSUJE TYLKO 1 GRACZA, TRZEBA ZMIENIC
 function drawPlayers() {
     let player1Img = new Image(SQUARE,SQUARE)
     player1Img.onload = function() {
-        ctxBuffer.drawImage(player1Img, LEFT_LINE + SQUARE + playersPos[0][0]*SQUARE, UPPER_LINE + SQUARE + playersPos[0][1]*SQUARE, this.width, this.height)
+        ctx.drawImage(player1Img, LEFT_LINE + SQUARE + playersPos[0][0]*SQUARE, UPPER_LINE + SQUARE + playersPos[0][1]*SQUARE, this.width, this.height)
 
     }
     player1Img.src = "sprites/player1.png"
 }
 
+// RYSUJE TŁO
+
 function drawBackground(sizeX, sizeY) {
     // GORNA LINIA
-    ctxBuffer.moveTo(0,UPPER_LINE)
-    ctxBuffer.lineTo(WIDTH,UPPER_LINE)
+    ctx.moveTo(0,UPPER_LINE)
+    ctx.lineTo(WIDTH,UPPER_LINE)
 
     // LEWY PANEL
-    ctxBuffer.moveTo(LEFT_LINE,UPPER_LINE)
-    ctxBuffer.lineTo(LEFT_LINE,HEIGHT)
+    ctx.moveTo(LEFT_LINE,UPPER_LINE)
+    ctx.lineTo(LEFT_LINE,HEIGHT)
 
     // PRAWY PANEL
-    ctxBuffer.moveTo(RIGHT_LINE,UPPER_LINE)
-    ctxBuffer.lineTo(RIGHT_LINE,HEIGHT)
+    ctx.moveTo(RIGHT_LINE,UPPER_LINE)
+    ctx.lineTo(RIGHT_LINE,HEIGHT)
 
     // GREEN BACKGROUND
-    ctxBuffer.fillStyle = "#23aa4b" // 35, 170, 75
-    ctxBuffer.fillRect(LEFT_LINE, UPPER_LINE, RIGHT_LINE-LEFT_LINE, HEIGHT-UPPER_LINE)
+    ctx.fillStyle = "#23aa4b" // 35, 170, 75
+    ctx.fillRect(LEFT_LINE, UPPER_LINE, RIGHT_LINE-LEFT_LINE, HEIGHT-UPPER_LINE)
 
     // WALL BLOCKS
     let wall = new Image(SQUARE,SQUARE)
@@ -83,32 +117,32 @@ function drawBackground(sizeX, sizeY) {
         for(let i=0;i<sizeY;i++) {
             let posX = LEFT_LINE
             let posY = UPPER_LINE + i*SQUARE
-            ctxBuffer.drawImage(wall, posX, posY, this.width, this.height)
+            ctx.drawImage(wall, posX, posY, this.width, this.height)
         }
 
         for(let i=0;i<sizeY;i++) {
             let posX = RIGHT_LINE - SQUARE
             let posY = UPPER_LINE + i*SQUARE
-            ctxBuffer.drawImage(wall, posX, posY, this.width, this.height)
+            ctx.drawImage(wall, posX, posY, this.width, this.height)
         }
 
         for(let i=0;i<sizeX;i++) {
             let posX = LEFT_LINE + i*SQUARE
             let posY = UPPER_LINE
-            ctxBuffer.drawImage(wall, posX, posY, this.width, this.height)
+            ctx.drawImage(wall, posX, posY, this.width, this.height)
         }
 
         for(let i=0;i<sizeX;i++) {
             let posX = LEFT_LINE + i*SQUARE
             let posY = HEIGHT - SQUARE
-            ctxBuffer.drawImage(wall, posX, posY, this.width, this.height)
+            ctx.drawImage(wall, posX, posY, this.width, this.height)
         }
 
         for(let i=0;i<(sizeX-2)/2;i++) {
             for(let j=0;j<(sizeY-2)/2;j++) {
                 let posX = LEFT_LINE + 2*SQUARE + i*2*SQUARE
                 let posY = UPPER_LINE + 2*SQUARE + j*2*SQUARE
-                ctxBuffer.drawImage(wall, posX, posY, this.width, this.height)
+                ctx.drawImage(wall, posX, posY, this.width, this.height)
             }
         }
 
