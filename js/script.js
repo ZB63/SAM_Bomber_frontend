d0f8cb7ea872432a37c64b418586177b4a5ad26b
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-08-30 14:36:55 +0200

    add double buffering

--- js/script.js
+++ js/script.js
@@ -13,6 +13,10 @@ const SQUARE = GAME_BOARD / boardSize
 let c = document.getElementById("myCanvas")
 let ctx = c.getContext("2d")
 
+let cBuffer = document.getElementById("myCanvas")
+cBuffer.width = WIDTH
+cBuffer.height = HEIGHT
+let ctxBuffer = c.getContext("2d")
 
 let playersPos = [
     [0, 0],
@@ -21,7 +25,8 @@ let playersPos = [
     [0, 0]
   ];
 
-window.setInterval(game,5)
+window.setInterval(game,10)
+
 
 document.addEventListener('keydown', function(event) {
     console.log("EELO")
@@ -41,13 +46,14 @@ function game() {
 
     drawBackground(boardSize,boardSize)
     drawPlayers()
+    ctx.drawImage(cBuffer)
 }
 
 // RYSUJE TYLKO 1 GRACZA
 function drawPlayers() {
     let player1Img = new Image(SQUARE,SQUARE)
     player1Img.onload = function() {
-        ctx.drawImage(player1Img, LEFT_LINE + SQUARE + playersPos[0][0]*SQUARE, UPPER_LINE + SQUARE + playersPos[0][1]*SQUARE, this.width, this.height)
+        ctxBuffer.drawImage(player1Img, LEFT_LINE + SQUARE + playersPos[0][0]*SQUARE, UPPER_LINE + SQUARE + playersPos[0][1]*SQUARE, this.width, this.height)
 
     }
     player1Img.src = "sprites/player1.png"
@@ -55,20 +61,20 @@ function drawPlayers() {
 
 function drawBackground(sizeX, sizeY) {
     // GORNA LINIA
-    ctx.moveTo(0,UPPER_LINE)
-    ctx.lineTo(WIDTH,UPPER_LINE)
+    ctxBuffer.moveTo(0,UPPER_LINE)
+    ctxBuffer.lineTo(WIDTH,UPPER_LINE)
 
     // LEWY PANEL
-    ctx.moveTo(LEFT_LINE,UPPER_LINE)
-    ctx.lineTo(LEFT_LINE,HEIGHT)
+    ctxBuffer.moveTo(LEFT_LINE,UPPER_LINE)
+    ctxBuffer.lineTo(LEFT_LINE,HEIGHT)
 
     // PRAWY PANEL
-    ctx.moveTo(RIGHT_LINE,UPPER_LINE)
-    ctx.lineTo(RIGHT_LINE,HEIGHT)
+    ctxBuffer.moveTo(RIGHT_LINE,UPPER_LINE)
+    ctxBuffer.lineTo(RIGHT_LINE,HEIGHT)
 
     // GREEN BACKGROUND
-    ctx.fillStyle = "#23aa4b" // 35, 170, 75
-    ctx.fillRect(LEFT_LINE, UPPER_LINE, RIGHT_LINE-LEFT_LINE, HEIGHT-UPPER_LINE)
+    ctxBuffer.fillStyle = "#23aa4b" // 35, 170, 75
+    ctxBuffer.fillRect(LEFT_LINE, UPPER_LINE, RIGHT_LINE-LEFT_LINE, HEIGHT-UPPER_LINE)
 
     // WALL BLOCKS
     let wall = new Image(SQUARE,SQUARE)
@@ -77,32 +83,32 @@ function drawBackground(sizeX, sizeY) {
         for(let i=0;i<sizeY;i++) {
             let posX = LEFT_LINE
             let posY = UPPER_LINE + i*SQUARE
-            ctx.drawImage(wall, posX, posY, this.width, this.height)
+            ctxBuffer.drawImage(wall, posX, posY, this.width, this.height)
         }
 
         for(let i=0;i<sizeY;i++) {
             let posX = RIGHT_LINE - SQUARE
             let posY = UPPER_LINE + i*SQUARE
-            ctx.drawImage(wall, posX, posY, this.width, this.height)
+            ctxBuffer.drawImage(wall, posX, posY, this.width, this.height)
         }
 
         for(let i=0;i<sizeX;i++) {
             let posX = LEFT_LINE + i*SQUARE
             let posY = UPPER_LINE
-            ctx.drawImage(wall, posX, posY, this.width, this.height)
+            ctxBuffer.drawImage(wall, posX, posY, this.width, this.height)
         }
 
         for(let i=0;i<sizeX;i++) {
             let posX = LEFT_LINE + i*SQUARE
             let posY = HEIGHT - SQUARE
-            ctx.drawImage(wall, posX, posY, this.width, this.height)
+            ctxBuffer.drawImage(wall, posX, posY, this.width, this.height)
         }
 
         for(let i=0;i<(sizeX-2)/2;i++) {
             for(let j=0;j<(sizeY-2)/2;j++) {
                 let posX = LEFT_LINE + 2*SQUARE + i*2*SQUARE
                 let posY = UPPER_LINE + 2*SQUARE + j*2*SQUARE
-                ctx.drawImage(wall, posX, posY, this.width, this.height)
+                ctxBuffer.drawImage(wall, posX, posY, this.width, this.height)
             }
         }
 
