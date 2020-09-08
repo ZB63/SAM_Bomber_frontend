20b2639a35e0b40fd35729c8966d30f24200e7dd
Simonsoft <szymonptak1993@gmail.com>
2020-09-08 11:02:18 +0200

    refactored DrawPlayers() method and bomb fixing in progress

--- js/script.js
+++ js/script.js
@@ -110,12 +110,6 @@ function onError(evt) {
 // ...
 // ...
 // DO MODYFIKACJI
-function handle_Explosion(message){
-    bomb_Explosion.uid = message.bomb_uid;
-    bomb_Explosion.x_range = message.x_range;
-    bomb_Explosion.y_range = message.y_range;
-    //bomb_Explosion.objects_hit = message.objects_hit;
-}
 
 function handleWelcomeMessage(message){
     boardSize = message.map_size_x;
@@ -160,15 +154,15 @@ function handleBombHasBeenPlanted(message) {
     bombs.push({ uid: message.bomb_uid, x: message.x , y: message.y })
 }
 
-// nie jestem pewien do czego to sluzy
-// function disconnect_Player(message){
-//     for(var i = 0; i < 4; i++){
-//         if(playersNicks[i] == message.nick){
-//             playersPos[i] = [message.x, message.y];
-//             playersNicks[i] = null;
-//         }
-//     }
-// }
+function handleBombExploded(message){
+    explosions.push({ 
+        uid: message.bomb_uid,
+        x_range: message.x_range, 
+        y_range: message.y_range,
+        objects_hit: JSON.parse(message.objects_hit) 
+    })
+    bombs.splice(message.uid, 1);
+}
 
 
 function onMessage(evt) {
@@ -184,6 +178,8 @@ function onMessage(evt) {
         handleBombAmount(message)
     } else if(message.msg_code === "Bomb has been planted") {
         handleBombHasBeenPlanted(message)
+    } else if(message.msg_code === "Bomb exploded"){
+        handleBombExploded(message)
     }
 
     game()
@@ -200,7 +196,6 @@ function drawBombs() {
                 ctx.drawImage(bombImage, LEFT_LINE + posX*SQUARE, UPPER_LINE + posY*SQUARE, this.width, this.height)
             }
         }
-
     }
     bombImage.src = "sprites/bomb.png"
 }
@@ -267,38 +262,17 @@ function drawBoxes() {
     boxImage.src = "sprites/box.png"
 }
 
-// RYSUJE TYLKO 1 GRACZA, TRZEBA ZMIENIC
 function drawPlayers() {
-
-        if(players[0].nick != null){
-            let player1Img = new Image(SQUARE,SQUARE)
-            player1Img.onload = function() {
-                ctx.drawImage(player1Img, LEFT_LINE + players[0].x*SQUARE, UPPER_LINE + players[0].y*SQUARE, this.width, this.height)
-            }
-            player1Img.src = "sprites/player1.png"
+    players.forEach(player => {
+        if (player.nick != null) {
+          let img = new Image(SQUARE, SQUARE)
+          img.onload = function() {
+            ctx.drawImage(img, LEFT_LINE + player.x * SQUARE, UPPER_LINE + player.y * SQUARE, this.width, this.height)
+          }
+          img.src = "sprites/player1.png"; // or `sprites/${player.image}`;
         }
-        if(players[1].nick != null){
-            let player2Img = new Image(SQUARE,SQUARE)
-            player2Img.onload = function() {
-                ctx.drawImage(player2Img, LEFT_LINE + players[1].x*SQUARE, UPPER_LINE + players[1].y*SQUARE, this.width, this.height)
-            }
-            player2Img.src = "sprites/player1.png"
-        }
-        if(players[2].nick != null){
-            let player3Img = new Image(SQUARE,SQUARE)
-            player3Img.onload = function() {
-                ctx.drawImage(player3Img, LEFT_LINE + players[2].x*SQUARE, UPPER_LINE + players[2].y*SQUARE, this.width, this.height)
-            }
-            player3Img.src = "sprites/player1.png"
-        }
-        if(players[3].nick != null){
-            let player4Img = new Image(SQUARE,SQUARE)
-            player4Img.onload = function() {
-                ctx.drawImage(player4Img, LEFT_LINE + players[3].x*SQUARE, UPPER_LINE + players[3].y*SQUARE, this.width, this.height)
-            }
-            player4Img.src = "sprites/player1.png"
-        }
-}
+      });
+  }
 
 // RYSUJE TŁO
 
